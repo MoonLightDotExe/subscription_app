55b424d5e9faaabdbe7d17d242de245f9fdc3626
Om Amonkar <ompamonkar@gmail.com>
2024-02-10 15:37:56 +0530

    Group Add and Join #2

--- backend/models/groups.models.js
+++ backend/models/groups.models.js
@@ -1,18 +1,29 @@
 const mongoose = require('mongoose')
 
 const groupSchema = new mongoose.Schema({
-    admin: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+  group_name: {
+    type: String,
+    required: [true, 'Please enter Group name'],
+  },
+  group_code: {
+    type: String,
+  },
+  admin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
-    users: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    subscriptions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Subscription'
-    }]
+  ],
+  subscriptions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Subscription',
+    },
+  ],
 })
 
 module.exports = mongoose.model('groups', groupSchema)
