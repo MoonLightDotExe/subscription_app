2620593e94a0168799fc0505362d193fbfe1b3cd
Aarush-Kinhikar-0209 <aarush.kinhikar@gmail.com>
2024-02-10 15:41:57 +0530

    groups.models.js splits

--- backend/models/groups.models.js
+++ backend/models/groups.models.js
@@ -17,20 +17,19 @@ const groupSchema = new mongoose.Schema({
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
     },
-    users: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    }],
-    splits: [{
-      contribution: [{
-        user: mongoose.Schema.Types.ObjectId,
-        amount: Number
-      }]
-    }],
-    subscriptions: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Subscription'
+  ],
+  splits: [{
+    contribution: [{
+      user: mongoose.Schema.Types.ObjectId,
+      amount: Number
     }]
+  }],
+  subscriptions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Subscription',
+    },
+  ],
 })
 
 module.exports = mongoose.model('groups', groupSchema)
