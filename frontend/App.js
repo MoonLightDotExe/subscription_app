50bc6c662a4490edda86d3e7cbbb8220bc6e4a91
Aarush-Kinhikar-0209 <aarush.kinhikar@gmail.com>
2024-02-10 22:33:24 +0530

    UI commit #2

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import VendorLanding from './components/VendorLanding'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST12!</Text>
+      {/* <Text>TEST12!</Text> */}
+      <VendorLanding />
       <StatusBar style='auto' />
     </View>
   )
