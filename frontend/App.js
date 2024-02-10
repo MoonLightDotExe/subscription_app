61fbafb14cb36ac98a16d5db4fa61a8051272737
Advika Kharat <kharatadvika@gmail.com>
2024-02-10 21:22:20 +0530

    UI commit #1

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./App/Screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./App/Screens/Auth/LoginScreen";
+import Register from "./App/Screens/Auth/Register";
+import Subs from "./App/Screens/Subs";
+import SelectSub from "./App/Screens/SelectSub";
+import SubDetails from "./App/components/subdetails";
+import Groups from "./App/Screens/Groups";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>TEST12!</Text>
-      <StatusBar style='auto' />
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="register" component={Register} />
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="subs" component={Subs} />
+        <Stack.Screen name="select" component={SelectSub} />
+        <Stack.Screen name="addnew" component={SubDetails} />
+        <Stack.Screen name="groups" component={Groups} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+});
---
+++ frontend/App/components/tab.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Assuming you're using React Navigation for navigation
+
+const TabComponent = ({ navigation, text, link }) => {
+
+
+
+    return (
+
+        <TouchableOpacity style={styles.tab} onPress={() => navigation.navigate(link)}>
+            <Text style={styles.text}>{text}</Text>
+        </TouchableOpacity>
+
+    );
+};
+
+const styles = StyleSheet.create({
+
+    tab: {
+        padding: 10,
+        marginHorizontal: 5,
+        backgroundColor: '#A3A3A3',
+        borderRadius: 5,
+        width: "80%",
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: 30
+    },
+    text: {
+        fontSize: 30
+    }
+
+
+});
+
+export default TabComponent;
