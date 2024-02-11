46a9eaa0407ef720f349fa8f705762990d0bf26b
Om Amonkar <ompamonkar@gmail.com>
2024-02-11 09:39:22 +0530

    Subs Update #1

--- frontend/App/Screens/Home.jsx
+++ frontend/App/Screens/Home.jsx
@@ -1,66 +1,88 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet, Button } from 'react-native';
-import TabComponent from '../components/tab';
+import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  Button,
+} from 'react-native'
+import TabComponent from '../components/tab'
 import netflix from './dn.jpg'
 import amazon from './subs-img.jpg'
 
 const Home = ({ navigation }) => {
-    return (
-        <ScrollView>
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.header}>SuperSubs</Text>
+        <Text style={styles.text}>
+          SuperSubs is a great subscription management tool. It aims to track,
+          optimize and collaborate for subscriptions !
+        </Text>
+        <TouchableOpacity style={{}}>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 30,
+              fontWeight: '300',
+              margin: 10,
+            }}
+          >
+            Exciting Offers
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.box}>
+          <Image
+            source={netflix}
+            style={{ width: '100%', height: 200, objectFit: 'fill' }}
+          />
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 20,
+            }}
+          >
+            Netflix + Disney+ - 50% off
+          </Text>
+        </View>
+        <View style={styles.box}>
+          <Image
+            source={amazon}
+            style={{ width: '100%', height: 200, objectFit: 'fill' }}
+          />
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 20,
+            }}
+          >
+            Amazon Prime + - 50% off
+          </Text>
+        </View>
 
-
-            <View style={styles.container}>
-
-                <Text style={styles.header}>SuperSubs</Text>
-                <Text style={styles.text}>SuperSubs is a great subscription management tool. It aims to track, optimize and collaborate for subscriptions !</Text>
-                <TouchableOpacity style={{}}>
-                    <Text style={{ color: "white", fontSize: 30, fontWeight: "300", margin: 10 }}>Exciting Offers</Text>
-
-                </TouchableOpacity>
-                <View style={styles.box}>
-                    <Image source={netflix} style={{ width: "100%", height: 200, objectFit: "fill" }} />
-                    <Text
-                        style={{
-                            color: "white",
-                            fontSize: 20,
-
-                        }}
-                    >Netflix + Disney+ - 50% off</Text>
-
-
-                </View>
-                <View style={styles.box}>
-                    <Image source={amazon} style={{ width: "100%", height: 200, objectFit: "fill" }} />
-                    <Text
-                        style={{
-                            color: "white",
-                            fontSize: 20,
-
-                        }}
-                    >Amazon Prime +  - 50% off</Text>
-
-
-                </View>
-
-
-                <View style={{
-                    position: 'absolute',
-                    bottom: 100,
-                    right: 20,
-                    backgroundColor: "#00FFC3",
-                    borderRadius: 10,
-                    padding: 5
-                }}>
-                    <TouchableOpacity style={{}} onPress={() => navigation.navigate('login')}>
-                        <Text style={{ color: "black", fontSize: 30, fontWeight: "300" }}>Login</Text>
-                    </TouchableOpacity>
-
-
-                </View>
-
-
-            </View>
-            {/* <TextInput
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 100,
+            right: 20,
+            backgroundColor: '#00FFC3',
+            borderRadius: 10,
+            padding: 5,
+          }}
+        >
+          <TouchableOpacity
+            style={{}}
+            onPress={() => navigation.navigate('login')}
+          >
+            <Text style={{ color: 'black', fontSize: 30, fontWeight: '300' }}>
+              Login
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* <TextInput
                 style={{
                     height: 40,
                     borderColor: 'gray',
@@ -68,53 +90,49 @@ const Home = ({ navigation }) => {
                 }}
                 defaultValue="You can type in me"
             /> */}
-
-
-        </ScrollView >
-    );
-};
+    </ScrollView>
+  )
+}
 
 const styles = StyleSheet.create({
-    box: {
-        borderWidth: 1,
-        borderColor: '#00FFC3', // Color of the border
-        borderRadius: 10, // Rounded border radius
-        padding: 20, // Padding inside the box
-        marginTop: 0,
-        width: "90%",
-        marginLeft: "auto",
-        marginRight: "auto"
-    },
-    header: {
-        fontSize: 50,
-        color: "#00FFC3",
-        margin: 10,
-        fontWeight: "100"
-    },
-    text: {
-        color: '#FFFFFF',
-        fontSize: 20,
-        fontWeight: '300',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 10
-
-    },
-    container: {
-        flex: 2,
-        margin: 0,
-        height: 800,
-        backgroundColor: "#000000", // Set background color to black
-
-    },
-    image: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    buttonContainer: {
-        position: 'absolute',
-        bottom: 20,
-        right: 20,
-    },
+  box: {
+    borderWidth: 1,
+    borderColor: '#00FFC3', // Color of the border
+    borderRadius: 10, // Rounded border radius
+    padding: 20, // Padding inside the box
+    marginTop: 0,
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  header: {
+    fontSize: 50,
+    color: '#00FFC3',
+    margin: 10,
+    fontWeight: '100',
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '300',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  container: {
+    flex: 2,
+    margin: 0,
+    height: 800,
+    backgroundColor: '#000000', // Set background color to black
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
 })
-export default Home;
+export default Home
--- frontend/App/Screens/Subs.jsx
+++ frontend/App/Screens/Subs.jsx
@@ -1,70 +1,78 @@
-import React from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
-import TabComponent from '../components/tab';
-import NavBar from '../components/navbar';
+import React from 'react'
+import { View, Button, StyleSheet, Text } from 'react-native'
+import TabComponent from '../components/tab'
+import NavBar from '../components/navbar'
 
 const Subs = ({ navigation }) => {
-    return (
+  return (
+    <View style={styles.container}>
+      <NavBar />
 
-        <View style={styles.container}>
-            <NavBar />
+      <Text style={styles.header}>My Subscriptions</Text>
+      <View style={styles.tabContainer}>
+        <TabComponent
+          text='Netflix'
+          link='home'
+          navigation={navigation}
+        />
+        <TabComponent
+          text='Amazon Prime'
+          link='home'
+          navigation={navigation}
+        />
+        <TabComponent
+          text='Disney+'
+          link='home'
+          navigation={navigation}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          color='#0DF3C9BC'
+          title='View Groups'
+          onPress={() => navigation.navigate('groups')}
+          style={styles.button}
+        />
 
-            <Text style={styles.header}>My Subscriptions</Text>
-            <View style={styles.tabContainer}>
-                <TabComponent text="Netflix" link="home" navigation={navigation} />
-                <TabComponent text="Amazon Prime" link="home" navigation={navigation} />
-                <TabComponent text="Disney+" link="home" navigation={navigation} />
-            </View>
-            <View style={styles.buttonContainer}>
-                <Button
-                    color="#0DF3C9BC"
-                    title="View Groups"
-                    onPress={() => navigation.navigate('groups')}
-                    style={styles.button}
-                />
-
-                <Button
-                    color="#0DF3C9C3"
-                    title="add new subscription"
-                    onPress={() => navigation.navigate('select')}
-                    style={styles.button}
-                />
-
-
-
-            </View>
-        </View>
-    );
+        <Button
+          color='#0DF3C9C3'
+          title='add new subscription'
+          onPress={() => navigation.navigate('select')}
+          style={styles.button}
+        />
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    header: {
-        fontSize: 40,
-        color: "#00FFC3",
-        margin: 10,
-        fontWeight: "100"
-    },
-    container: {
-        flex: 1,
-        backgroundColor: "black"
-    },
-    tabContainer: {
-        flexDirection: 'column',
-        marginBottom: 20,
-    },
-    buttonContainer: {
-        position: 'absolute',
-        bottom: 20,
-        right: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 40
-    },
-    button: {
-        fontSize: 20, // Increase the font size
-        paddingVertical: 10, // Adjust the vertical padding
-        paddingHorizontal: 20, // Adjust the horizontal padding
-    },
-});
+  header: {
+    fontSize: 40,
+    color: '#00FFC3',
+    margin: 10,
+    fontWeight: '100',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  tabContainer: {
+    flexDirection: 'column',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 40,
+  },
+  button: {
+    fontSize: 20, // Increase the font size
+    paddingVertical: 10, // Adjust the vertical padding
+    paddingHorizontal: 20, // Adjust the horizontal padding
+  },
+})
 
-export default Subs;
+export default Subs
